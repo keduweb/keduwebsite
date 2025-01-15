1af26be46776d231544b4fb03701f7551093a53b
keduweb <mybef4566@gmail.com>
2025-01-15 22:21:49 +0900

    Initial commit with login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,64 +1,63 @@
 import React, { useState } from 'react';
 import './LoginForm.css';
 
-function LoginForm({ onSwitchTab, onLoginSuccess, users }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const LoginForm = ({ onLoginSuccess }) => {
+  const [nickname, setNickname] = useState('');
+  const [yaoongCode, setYaoongCode] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
-    const user = users.find((user) => user.username === username);
+    const nicknameRegex = /^[a-zA-Z가-힣]{1,10}$/;
+    const validYaoongCode = '20240905';
 
-    if (user && user.password === password) {
-      alert('로그인 성공!');
-      onLoginSuccess();
-    } else {
-      alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+    // Validate Yaoong Code
+    if (yaoongCode !== validYaoongCode) {
+      setErrorMessage('야옹이 코드가 일치하지 않습니다.');
+      return;
     }
+
+    // Validate Nickname
+    if (!nicknameRegex.test(nickname)) {
+      setErrorMessage('닉네임은 한영 조합 10자 이내여야 합니다.');
+      return;
+    }
+
+    // Successful login
+    setErrorMessage('');
+    onLoginSuccess();
   };
 
   return (
     <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h2>로그인</h2>
+      <form className="login-form" onSubmit={handleLogin}>
+        <h2>야옹이랑놀래?</h2>
         <div className="form-group">
-          <label>아이디</label>
+          <label>닉네임</label>
           <input
             type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="아이디를 입력하세요"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+            maxLength="10"
             required
           />
+          <small>한영 조합 10자 이내</small>
         </div>
         <div className="form-group">
-          <label>비밀번호</label>
+          <label>야옹이 코드</label>
           <input
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호를 입력하세요"
+            value={yaoongCode}
+            onChange={(e) => setYaoongCode(e.target.value)}
             required
           />
         </div>
+        {errorMessage && <p style={{ color: 'red', textAlign: 'center' }}>{errorMessage}</p>}
         <button type="submit" className="login-button">로그인</button>
-        <div className="tabs">
-          <span onClick={() => onSwitchTab('findAccount')} className="tab-link">
-            아이디 찾기
-          </span>{' '}
-          |{' '}
-          <span onClick={() => onSwitchTab('findAccount')} className="tab-link">
-            비밀번호 찾기
-          </span>{' '}
-          |{' '}
-          <span onClick={() => onSwitchTab('signup')} className="tab-link">
-            회원가입
-          </span>
-        </div>
       </form>
     </div>
   );
-}
+};
 
 export default LoginForm;
